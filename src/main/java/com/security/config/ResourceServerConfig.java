297797ddf6805ab56bfdca5b27f0484cba46cdad
Tiago Pereira <tiago.pereira@hotmart.com>
2022-10-30 00:25:33 -0300

    Utilizando autenticação JWT com chave simétrica

--- src/main/java/com/security/config/ResourceServerConfig.java
+++ src/main/java/com/security/config/ResourceServerConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +20,13 @@ public class ResourceServerConfig {
                 .anyRequest().authenticated()
                 .and()
                 .cors().and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
         return http.build();
+    }
+    
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s".getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 }
