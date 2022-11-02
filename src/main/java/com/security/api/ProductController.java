0f62e7c522b220d0a5506de2638878ce87135630
Tiago Pereira <tiago.pereira@hotmart.com>
2022-11-02 01:11:20 -0300

    Carregando as Granted Authorities e restringindo acesso a endpoints na API

--- src/main/java/com/security/api/ProductController.java
+++ src/main/java/com/security/api/ProductController.java
@@ -1,15 +1,29 @@
 package com.security.api;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping(path = "/v1/products", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductController {
     
-    @RequestMapping
+    @GetMapping
     public ResponseEntity<String> getProduct() {
-        return ResponseEntity.ok("Ola mundo!");
+        return ResponseEntity.ok("Get product");
+    }
+    
+    @PostMapping
+    public ResponseEntity<String> addProduct() {
+        return ResponseEntity.ok("Add product");
+    }
+    
+    @PutMapping
+    public ResponseEntity<String> updateProduct() {
+        return ResponseEntity.ok("Update product");
     }
 }
--- src/main/java/com/security/core/ResourceServerConfig.java
+++ src/main/java/com/security/core/ResourceServerConfig.java
@@ -2,9 +2,15 @@ package com.security.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -13,10 +19,30 @@ public class ResourceServerConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers(HttpMethod.POST, "/v1/products/**").hasAuthority("GERENCIAR_PEDIDOS")
+                .antMatchers(HttpMethod.PUT, "/v1/products/**").hasAuthority("GERENCIAR_PEDIDOS")
+                .antMatchers(HttpMethod.GET, "/v1/products/**").authenticated()
+                .anyRequest().denyAll()
                 .and()
                 .cors().and()
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
         return http.build();
+    }
+    
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = jwt.getClaimAsStringList("authorities");
+            
+            if (authorities == null) {
+                authorities = Collections.emptyList();
+            }
+            
+            return authorities.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+        return jwtAuthenticationConverter;
     }
 }
