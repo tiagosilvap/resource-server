fffc640d28c1f11f20c34a724f03b0c6f3803398
Tiago Pereira <tiago.pereira@hotmart.com>
2022-11-18 00:49:21 -0300

    Atualizando nomes de escopo

--- src/main/java/com/security/api/ProductController.java
+++ src/main/java/com/security/api/ProductController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,32 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/v1/products", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v1/products", produces = MediaType.TEXT_PLAIN_VALUE)
 public class ProductController {
     
     @Autowired
     private AppSecurity appSecurity;
     
-    @PreAuthorize("@appSecurity.userDebora")
-    @GetMapping
+    @PreAuthorize("hasAuthority('GERENCIAR_PEDIDOS') or @appSecurity.userDebora")
+    @GetMapping(produces=MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> getProduct() {
         System.out.println(appSecurity.getAuthentication().getAuthorities());
         return ResponseEntity.ok("Get product");
     }
     
-    @PreAuthorize("hasAuthority('SCOPE_read') and hasAuthority('SCOPE_write') and hasAuthority('GERENCIAR_PEDIDOS')")
+    @PreAuthorize("hasAuthority('SCOPE_READ') and hasAuthority('SCOPE_WRITE') and hasAuthority('GERENCIAR_PEDIDOS')")
     @PostMapping
     public ResponseEntity<String> addProduct() {
         return ResponseEntity.ok("Add product");
     }
     
-    @PreAuthorize("hasAuthority('SCOPE_write') and hasAuthority('EDITAR_PRODUTOS')")
+    @PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDITAR_PRODUTOS')")
     @PutMapping
     public ResponseEntity<String> updateProduct() {
         return ResponseEntity.ok("Update product");
     }
     
-    @PreAuthorize("hasAuthority('SCOPE_read') and isAuthenticated()")
+    @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
     @DeleteMapping
     public ResponseEntity<String> deleteProduct() {
         return ResponseEntity.ok("Delete product");
